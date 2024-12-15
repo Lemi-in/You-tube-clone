ae3ceff711b92045b2a78fc3347e7126a166bfb2
*Lemi-in* <*lemesaelias@gmail.com*>
2024-12-15 15:13:33 +0300

    made some changes..

--- CLIENT/src/main.js
+++ CLIENT/src/main.js
@@ -1,18 +1,1 @@
-// This is the main javascript file that we will be working on 
-const videoGrid = document.getElementById('videoGrid');
-const videos = [
-  { title: 'Video 1', channel: 'Channel A' },
-  { title: 'Video 2', channel: 'Channel B' },
-  { title: 'Video 3', channel: 'Channel C' },
-  { title: 'Video 4', channel: 'Channel D' }
-];
-videos.forEach(video => {
-  const card = document.createElement('div');
-  card.className = 'video-card';
-  card.innerHTML = `
-    <div class="video-thumbnail"></div>
-    <div class="video-title">${video.title}</div>
-    <div class="video-channel">${video.channel}</div>
-  `;
-  videoGrid.appendChild(card);
-});
+//This is the main javascript file that we will be working on in the future
